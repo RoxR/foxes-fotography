7bcb4dfb550b453374639896923aba5a2352a097
Sean Kennedy <cspheregreen@gmail.com>
2017-08-15 08:03:03 +0300

    nav links plus dat font tho

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+  def livereload_tag
+    # get your ip on your local network
+    local_ip = Socket.ip_address_list.detect(&:ipv4_private?).ip_address
+    # livereload listen port
+    port = '35729'
+    javascript_include_tag "http://#{local_ip}:#{port}/livereload.js?snipver=1"
+  end
 end
