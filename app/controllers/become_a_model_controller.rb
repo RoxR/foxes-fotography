a8987b2efc20af3636ee4a05c33cea2f473523b4
Sean Kennedy <cspheregreen@gmail.com>
2017-08-18 23:32:36 +0300

    move model from to partial.

--- app/controllers/become_a_model_controller.rb
+++ app/controllers/become_a_model_controller.rb
@@ -3,45 +3,45 @@ class BecomeAModelController < ApplicationController
   end
 
   def create
-  	@model = Model.new(become_a_model_params)
-  	if @model.save
-  		# Flash not implemented yet, logging into console
-  		puts 'Model saved succesfully'
-  		redirect_to submitted_path
-  	else
-  		puts 'There was an error saving a model'
-  		redirect_to bcome_a_model_path
-  	end
+    @model = Model.new(become_a_model_params)
+    if @model.save
+      # Flash not implemented yet, logging into console
+      puts 'Model saved succesfully'
+      redirect_to submitted_path
+    else
+      puts 'There was an error saving a model'
+      redirect_to bcome_a_model_path
+    end
   end
 
   private
 
   def become_a_model_params
-  	params.permit(
-  		:name,
+    params.permit(
+      :name,
       :email,
       :phone_number, 
-  		:gender,
-  		:height, 
-  		:weight, 
-  		:bust, 
-  		:waist, 
-  		:hips, 
-  		:cup, 
-  		:dress,
-  		:shoe,
-  		:hair_length,
-  		:eye_color,
-  		:ethnicity, 
-  		:skin_color,
-  		:nudes,
-  		:tattoos,
-  		:piercings, 
-  		:experience,
-  		:compensation,
-  		:country,
+      :gender,
+      :height, 
+      :weight, 
+      :bust, 
+      :waist, 
+      :hips, 
+      :cup, 
+      :dress,
+      :shoe,
+      :hair_length,
+      :eye_color,
+      :ethnicity, 
+      :skin_color,
+      :nudes,
+      :tattoos,
+      :piercings, 
+      :experience,
+      :compensation,
+      :country,
       :hair_color
-  	)
+    )
   end
 
 
