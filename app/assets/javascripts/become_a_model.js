ab30196a20d615a5b3a62368d5e3dad215c67615
valdasvabolis <thisisvaldasvabolis@gmail.com>
2017-08-19 23:18:57 +0300

    Added styles and previews for multi-image uploads

--- app/assets/javascripts/become_a_model.js
+++ app/assets/javascripts/become_a_model.js
@@ -1,7 +1,29 @@
 $(document).ready(function(){
-    var preview = $(".img-placeholder img[data-id='1']");
     
-    $("#picture").change(function(event){
+    $('#picture_1').change(function(event){
+       var preview = $(".picture img[data-id='1']");
+       var input = $(event.currentTarget);
+       var file = input[0].files[0];
+       var reader = new FileReader();
+       reader.onload = function(e){
+           image_base64 = e.target.result;
+           preview.attr("src", image_base64);
+       };
+       reader.readAsDataURL(file);
+    });
+    $('#picture_2').change(function(event){
+       var preview = $(".picture img[data-id='2']");
+       var input = $(event.currentTarget);
+       var file = input[0].files[0];
+       var reader = new FileReader();
+       reader.onload = function(e){
+           image_base64 = e.target.result;
+           preview.attr("src", image_base64);
+       };
+       reader.readAsDataURL(file);
+    });
+    $('#picture_3').change(function(event){
+       var preview = $(".picture img[data-id='3']");
        var input = $(event.currentTarget);
        var file = input[0].files[0];
        var reader = new FileReader();
