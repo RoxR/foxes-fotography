a70fc0a23cd8fa17183ff6bfac4f59a0f8860062
valdasvabolis <thisisvaldasvabolis@gmail.com>
2017-08-20 13:43:30 +0300

    Fixed formatting

--- app/controllers/become_a_model_controller.rb
+++ app/controllers/become_a_model_controller.rb
@@ -3,45 +3,46 @@ class BecomeAModelController < ApplicationController
   end
 
   def create
-  	@model = Model.new(become_a_model_params)
+    @model = Model.new(become_a_model_params)
     @model.pictures.new(picture_params)
-  	if @model.save
-  		puts 'Model saved succesfully'
-  		redirect_to submitted_path
-  	else
-  		puts 'There was an error saving a model'
-  		redirect_to bcome_a_model_path
-  	end
+
+    if @model.save
+      puts 'Model saved succesfully'
+      redirect_to submitted_path
+    else
+      puts 'There was an error saving a model'
+      redirect_to bcome_a_model_path
+    end
   end
 
   private
 
   def become_a_model_params
-  	params.permit(
-  		:name,
+    params.permit(
+      :name,
       :email,
       :phone_number, 
-  		:gender,
-  		:height, 
-  		:weight, 
-  		:bust, 
-  		:waist, 
-  		:hips, 
-  		:cup, 
-  		:dress,
-  		:shoe,
-  		:hair_length,
-  		:eye_color,
-  		:ethnicity, 
-  		:skin_color,
-  		:nudes,
-  		:tattoos,
-  		:piercings, 
-  		:experience,
-  		:compensation,
-  		:country,
+      :gender,
+      :height, 
+      :weight, 
+      :bust, 
+      :waist, 
+      :hips, 
+      :cup, 
+      :dress,
+      :shoe,
+      :hair_length,
+      :eye_color,
+      :ethnicity, 
+      :skin_color,
+      :nudes,
+      :tattoos,
+      :piercings, 
+      :experience,
+      :compensation,
+      :country,
       :hair_color
-  	)
+    )
   end
 
   def picture_params
