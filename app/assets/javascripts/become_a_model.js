eb65e5ae07b147ef2a426aedccec1ef286862121
valdasvabolis <thisisvaldasvabolis@gmail.com>
2017-08-22 00:18:52 +0300

    Added server and client side file size restrictions

--- app/assets/javascripts/become_a_model.js
+++ app/assets/javascripts/become_a_model.js
@@ -1,12 +1,26 @@
 $(document).on('turbolinks:load', function() {
+
+  var LIMIT_SIZE_MB = 10
+  
+  function isFileTooLarge(file) {
+    return file.size / Math.pow(1024,2) > LIMIT_SIZE_MB
+  }
+
+  var picture_too_big = false;
   $('#become_a_model input[type=file]').change(function(e) {
     var input = $(e.currentTarget);
-    var preview = input.closest('.img-placeholder').find('img');
-    var reader = new FileReader();
-    reader.onload = function(ev) {
-      preview.attr("src", ev.target.result);
-    };
-    reader.readAsDataURL(input[0].files[0]);
+    var file = input[0].files[0];
+    if(isFileTooLarge(file)) {
+      alert('Picture size too big (over ' + LIMIT_SIZE_MB + ' MB)');
+      input.val('');
+    } else {
+      var preview = input.closest('.img-placeholder').find('img');
+      var reader = new FileReader();
+      reader.onload = function(ev) {
+        preview.attr("src", ev.target.result);
+      };
+      reader.readAsDataURL(file);
+    }
   });
   $('#form-submit-btn').on('click', function(e) {
     e.preventDefault();
--- app/models/picture.rb
+++ app/models/picture.rb
@@ -1,4 +1,9 @@
 class Picture < ApplicationRecord
+  validates_size_of :picture_1, maximum: 10.megabytes, message: "Picture 1 is over 10 megabytes"
+  validates_size_of :picture_2, maximum: 10.megabytes, message: "Picture 2 is over 10 megabytes"
+  validates_size_of :picture_3, maximum: 10.megabytes, message: "Picture 3 is over 10 megabytes"
+  validates_size_of :picture_preview, maximum: 10.megabytes, message: "Picture preview is over 10 megabytes"
+
   mount_uploader :picture_1, PictureUploader
   mount_uploader :picture_2, PictureUploader
   mount_uploader :picture_3, PictureUploader
